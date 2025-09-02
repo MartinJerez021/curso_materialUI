e027873f09d9eefe51e682c615f9e56308c394a1
MartinJerez021 <jerezmartin2002@gmail.com>
2025-09-02 16:38:28 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Typography} from '@mui/material'
+export default function App () {
+    return(
+        <div>
+            <Typography variant='h1'>
+                Martín Jerez
+            </Typography>
+        </div>
+    )
+}
